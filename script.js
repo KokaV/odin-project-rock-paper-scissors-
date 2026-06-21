1f1e42085b9a732dfb1f36f9dbe942a04c0a6640
KokaV <kokavashakidze@gmail.com>
2026-06-21 19:17:05 +0400

    step 3 completed - getHumanChoice function added

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+function getComputerChoice() {
+    let randomNumber = Math.random();
+
+    if (randomNumber < 0.33) {
+        return "rock";
+    } else if (randomNumber < 0.66) {
+        return "paper";
+    } else {
+        return  "scissors";
+    }    
+}
+console.log(getComputerChoice());
+
+function getHumanChoice() {
+    let humanChoice = prompt("Choose from: Rock, Paper, Scissors");
+    return humanChoice;
+}
+console.log(getHumanChoice());
